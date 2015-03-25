bf1da9e3ce6050d8b1658c056f7703957871239e
Rachel Geistfeld <rachel.geistfeld@gmail.com>
2015-03-25 05:49:00 +0000

    Added a footer and a working submit button

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,19 @@
 class PlayersController < ApplicationController
 	def index
+		@player = Player.order("RANDOM()").first
+		@player2 = Player.order("RANDOM()").first
+	end
+
+	def new
+		@player = Player.new
+	end
+
+	def create
+		Player.create(player_params)
+		redirect_to root_path
+	end
+
+	def player_params
+		params.require(:player).permit(:name)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rpxd::Application.routes.draw do
   root 'players#index'
+  resources :players
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
